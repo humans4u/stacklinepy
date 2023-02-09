8b34b19f0e913c26fbd86006587e7039fbf19e09
asaez <asaez@icc.ub.edu>
2023-02-09 19:31:10 +0100

    pushing initial skels

--- setup.py
+++ setup.py
@@ -7,7 +7,19 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='stackline',
-    version='0.0.1',
-    license=''
-    python_requires='>=3.10.9'
+    version='0.0.1.dev1',
+    license='GPL-3.0',
+    python_requires='>=3.10.9',
+    description='Simple pipeline management implemented in python.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    packages=['stackline'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+    project_urls={
+        'Source': 'https://github.com/humans4u/stacklinepy/'
+    }
 )
